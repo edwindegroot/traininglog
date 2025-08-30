1d06d6d3dcfa6d16443ce3f149ee3d02d9f90353
Edwin de Groot <edwindegroot93@gmail.com.com>
2025-08-30 23:22:01 +0200

    Basic spring setup

---
+++ src/main/java/com/edwin/traininglog/TraininglogApplication.java
@@ -0,0 +1,13 @@
+package com.edwin.traininglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraininglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraininglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edwin/traininglog/TraininglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.traininglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraininglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
