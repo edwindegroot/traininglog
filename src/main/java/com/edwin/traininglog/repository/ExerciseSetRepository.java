41097b0415738325827715a9c16b2c45a172b753
Edwin de Groot <edwindegroot93@gmail.com.com>
2025-09-08 17:30:04 +0200

    Cleanup; remove separate ExerciseSet service

--- src/main/java/com/edwin/traininglog/repository/ExerciseSetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edwin.traininglog.repository;
-
-import com.edwin.traininglog.entity.ExerciseSet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExerciseSetRepository extends JpaRepository<ExerciseSet, Long> {
-}
